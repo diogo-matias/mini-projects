febd36e1e87272d7236c167dd0c006fb3460f533
diogo-pc <diogomaccedo200@gmail.com>
2022-04-26 17:49:49 -0300

    JS-Traffic-Light

---
+++ traffic-light/javaScript.js
@@ -0,0 +1,60 @@
+const buttons = document.getElementById("buttons");
+const images = document.getElementById("img");
+let indexColors = null;
+let setIntervalID = null;
+
+console.log(buttons);
+console.log(images);
+
+const trafficLight = (evento) => {
+  stopInterval();
+  turnOn[evento.target.id]();
+};
+
+const controlaIndiceColors = () => {
+  if (indexColors < 2) {
+    indexColors++;
+  } else {
+    indexColors = 0;
+  }
+
+  return indexColors;
+};
+
+const changeColors = () => {
+  controlaIndiceColors();
+  const color = ["red", "yellow", "green"];
+  turnOn[color[indexColors]]();
+
+  let mostrarCorNoConsole = null;
+  switch (indexColors) {
+    case 0:
+      ShowinConsole = "red";
+      break;
+    case 1:
+      ShowinConsole = "yellow";
+      break;
+    case 2:
+      ShowinConsole = "green";
+      break;
+
+    default:
+      break;
+  }
+  console.log(ShowinConsole);
+};
+
+const stopInterval = () => {
+  clearInterval(setIntervalID);
+};
+
+const turnOn = {
+  red: () => (img.src = "./img/vermelho.png"),
+  yellow: () => (images.src = "./img/amarelo.png"),
+  green: () => (images.src = "./img/verde.png"),
+  automatic: () => {
+    setIntervalID = setInterval(changeColors, 1000);
+  },
+};
+
+buttons.addEventListener("click", trafficLight);
